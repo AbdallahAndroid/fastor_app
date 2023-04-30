233445125952d99dbcf78965bb928dd61eb0e456
AbdallahDafa <abdallah.mahmoud@dafa.sa>
2023-04-30 15:51:05 +0300

    button compare code done

--- lib/tutorial/button/TutorialButtonScreen.dart
+++ lib/tutorial/button/TutorialButtonScreen.dart
@@ -1,54 +1,75 @@
 import 'package:fastor_app_ui_widget/fastor_app_ui_widget.dart';
 import 'package:flutter/material.dart';
 
-class TutorialButtonScreen extends StatefulWidget {
+import '../../resource/resourceProject/FontProject.dart';
 
+class TutorialButtonScreen extends StatefulWidget {
   @override
   TutorialButtonState createState() => TutorialButtonState();
-
 }
 
 class TutorialButtonState extends State<TutorialButtonScreen> {
-
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold( body: SafeArea( child: getContent() ) );
+    return Scaffold(body: SafeArea(child: getContent()));
   }
-
 
   Widget getContent() {
-    return Column( children: [
-      buttonNormal(),
-      buttonFastor(),
-      TextFastor( "h1", fontSize: 15,),
-      Text( "h2")
-    ],
-    mainAxisSize: MainAxisSize.max,
-    mainAxisAlignment: MainAxisAlignment.center,
-    crossAxisAlignment: CrossAxisAlignment.center,);
-  }
+    return Center(child: Column(
+      children: [
 
+        buttonNormal(),
+        Placeholder( color: Colors.black, fallbackHeight: 1,),
+        buttonFastor(),
+        Placeholder( color: Colors.black, fallbackHeight: 1,),
+      ],
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+    ));
+  }
 
   Widget buttonNormal() {
-    return   ElevatedButton(onPressed:(){
-      print( "click on btn type normal");
-    },
-      child: Text('Normal Btnnnnnnnnnnnnnnn'), );
-  }
-
-
-  Widget buttonFastor() {
-    return ButtonFastor( "Fastor Btn", () {
-      print( "click on btn type fastor");
-    } ,
-      textColor: HexColor("#000000"),
-      background: HexColor("#999999"),
-      // textFontSize: 15,
-      // height: 35,
-
+    return Container(
+      margin: EdgeInsets.symmetric( vertical: 30),
+      child: ElevatedButton(
+          onPressed: () {
+            print("click on btn type normal");
+          },
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.black,
+            shape: RoundedRectangleBorder (
+              borderRadius: BorderRadius.circular(15),
+              side: BorderSide(width: 1,color: Colors.blue)
+          )),
+          child: Text(
+            "Button Normal",
+            textAlign: TextAlign.center,
+            maxLines: 2,
+            style: TextStyle(
+                color: Colors.blue,
+                fontSize: 15,
+                fontFamily: FontProject.marina
+            ),
+          )
+      ),
+      // color: Colors.green,
     );
   }
 
-
+  Widget buttonFastor() {
+    return ButtonFastor(
+      "Button Fastor",
+      () {
+        print("click on btn type fastor");
+      },
+      margin: EdgeInsets.symmetric( vertical: 40),
+      textColor: Colors.blue,
+      background: Colors.black,
+      fontFamily: FontProject.marina,
+      textFontSize: 15,
+      borderLine: Colors.blue,
+      borderRadius: 15,
+    );
+  }
 }
