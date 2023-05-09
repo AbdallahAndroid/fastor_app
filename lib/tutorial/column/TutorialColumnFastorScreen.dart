a94c638592f7b39eee08c58a4ab54374cd45ec19
AbdallahDafa <abdallah.mahmoud@dafa.sa>
2023-05-09 17:40:30 +0300

    columnFastor

--- lib/tutorial/column/TutorialColumnFastorScreen.dart
+++ lib/tutorial/column/TutorialColumnFastorScreen.dart
@@ -9,29 +9,66 @@ class TutorialColumnFastorScreen extends StatefulWidget {
 class _TutorialColumnFastorState extends State<TutorialColumnFastorScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: SafeArea(child: typeFastorColumn()));
+    return PageFastor(this , content: compareWidget() );
+    // return PageFastor( this , content: typeFastorColumn() );
   }
+
+  Widget compareWidget(){
+    return Center(child: Column(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+
+        /**
+            TextFastor( "Type Normal", margin: EdgeInsets.only(top: 30),),
+            typeNormalColumn(),
+            const Placeholder( color: Colors.black, fallbackHeight: 1,),
+
+         */
+
+        TextFastor( "Type Fastor", margin: EdgeInsets.only(top: 30),),
+        typeFastorColumn(),
+        const Placeholder( color: Colors.black, fallbackHeight: 1,),
+      ],
+    ));
+  }
+
+
+  Widget typeNormalColumn() {
+    return Container(
+        margin: EdgeInsets.only(top: 20, bottom: 20, left: 60, right: 60),
+        padding: EdgeInsets.all(5),
+        decoration: BoxDecoration(
+            border: Border.all(color: Colors.red),
+            borderRadius: BorderRadius.all(Radius.circular(15)),
+            // color: Colors.red,
+            color: Colors.yellow //background color
+        ),
+        child:  Column(
+            children: getChildren()
+        )
+    ) ;
+  }
+
 
   Widget typeFastorColumn() {
     return ColumnFastor(
         children: getChildren(),
-        margin: EdgeInsets.only(top: 40, left: 60, right: 60),
+        margin: EdgeInsets.only(top: 20, bottom: 20, left: 60, right: 60),
         padding: EdgeInsets.all(5),
-        // mainAxisAlignment: MainAxisAlignment.start,
-        // mainAxisSize : MainAxisSize.min,
-        // crossAxisAlignment: CrossAxisAlignment.start,
-      colorBackground: Colors.black,
-        // decoration: BoxDecoration(
-        //     border: Border.all(color: Colors.red),
-        //     borderRadius: BorderRadius.all(Radius.circular(15)),
-        //     color: Colors.yellow //background color
-        //     )
+        decoration: BoxDecoration(
+            border: Border.all(color: Colors.red),
+            borderRadius: BorderRadius.all(Radius.circular(15)),
+            color: Colors.yellow //background color
+        ),
     );
   }
 
+
   List<Widget> getChildren() {
     List<Widget> data = [];
-    for (int i = 1; i <= 7; i++) {
+    for (int i = 3; i <= 7; i++) {
       var w = ButtonFastor(
         "Data $i",
         textFontSize: 15,
---
+++ tutorial/column/README.md
@@ -0,0 +1,33 @@
+## Tutorial : ColumnFastor
+
+### Feature Fastor 
+* Have Decoration
+* Have Space : Margin, Padding
+* Have Alignment
+* Can Set Fixed size width/height
+
+
+### ColumnFastor
+
+Using Fastor widget Scroll Horizontal for get ride of RenderFlex overflowed by pixels
+
+<img src="https://raw.githubusercontent.com/AbdallahAndroid/fastor_app/master/tutorial/column/thamp.png" 
+ height="300"/>
+
+See source code compare between Fastor and Normal at this page
+
+```
+    ColumnFastor(
+        children: getChildren(),
+        margin: EdgeInsets.only(top: 20, bottom: 20, left: 60, right: 60),
+        padding: EdgeInsets.all(5),
+        decoration: BoxDecoration(
+            border: Border.all(color: Colors.red),
+            borderRadius: BorderRadius.all(Radius.circular(15)),
+            color: Colors.yellow //background color
+        ),
+    );
+```
+ 
+
+<hr class="solid">
